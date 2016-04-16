1b89b6d6ff7e61526c2f282aad6363bd912a3e0f
Tim <thung1@binghamton.edu>
2016-04-16 18:57:50 -0500

    Try sending generic card messages

--- server/routes/fb.js
+++ server/routes/fb.js
@@ -37,6 +37,56 @@ function sendTextMessage(sender, text) {
   })
 }
 
+function sendGenericMessage(sender) {
+  messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com",
+            "title": "web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        }, {
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
+
 router.post('/webhook/', function (req, res) {
   messaging_events = req.body.entry[0].messaging
     for (i = 0; i < messaging_events.length; i++) {
@@ -44,6 +94,10 @@ router.post('/webhook/', function (req, res) {
         sender = event.sender.id
         if (event.message && event.message.text) {
           text = event.message.text
+            if(text === 'Generic') {
+                sendGenericMessage(sender);
+                continue;
+            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
         }
     }
