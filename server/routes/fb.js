73824a068f523fa27b1e9215578f1d82ef06a0c6
Tim <thung1@binghamton.edu>
2016-04-16 18:23:36 -0500

    Fix callback for webhook

--- server/routes/fb.js
+++ server/routes/fb.js
@@ -4,12 +4,12 @@ var request = require('request');
 
 // Index route
 router.get('/', function (req, res) {
-  res.send('Hello world, I am a chat bot');
+  res.send('16479454');
 });
 
 // for Facebook verification
 router.get('/webhook/', function (req, res) {
-  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+  if (req.query['hub.verify_token'] === 'eleven_eaton_pl') {
     res.send(req.query['hub.challenge']);
   }
   res.send('Error, wrong token');
